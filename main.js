b39f7f390f20e387511788f74baa5c2ae4ad60ae
cauaameno <106753236+cauaameno@users.noreply.github.com>
2024-03-14 07:55:23 -0300

    ales

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ for (let i = 0; i < botoes.length; i++)  {
       botoes[i].onclik = function () {
 
         for (let j = 0; j <botoes.length; j++) {
-            botoes [j].classList.add("ativo");
+            botoes [j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
